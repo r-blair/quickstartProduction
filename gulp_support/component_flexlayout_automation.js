74b673e5f29a94b7ecb82312146e509939ca1639
r-blair <blairr@alumni.uleth.ca>
2017-04-17 21:15:57 -0700

    more progress

--- gulp_support/component_flexlayout_automation.js
+++ gulp_support/component_flexlayout_automation.js
@@ -1,20 +1,5 @@
 var fs = require("fs");
 var path = require("path");
-//var vinyl = require("vinyl");
-
-// STRINGS AND OBJECTS FOR TESTING.
-
-// const testHtmlSegment = "<div class=\"lerp gerp berp chirp tits-container tits-item\">";
-
-// const testVinyl = new vinyl({
-//   cwd: process.cwd(),
-//   base: __dirname,
-//   path: __filename,
-//   contents: null
-// });
-
-// console.log(`${testVinyl.cwd} \n ${testVinyl.base} \n ${testVinyl.path}`);
-
 
 // FUNCTIONS
 // Gradually building up complexity by composition of basic functions.
@@ -34,10 +19,22 @@ function nameOfComponent(file){
   return path.parse(file.path).name.slice(0,-10);
 }; // TESTED AND WORKS
 
+function nameOfClass(string){
+
+};
+
+function findSuffix(suffix){
+  return function(string){
+    return string.includes(suffix) ? string : void 0;
+  };
+};
+
+const findItemClass = findSuffix("item");
+const findContainerClass = findSuffix("container");
+
 function htmlLineHasClass(string){
   return string.includes("class=");
 }; // TESTED AND WORKS
-
 
 function retrieveClassString(string){
 
@@ -53,11 +50,11 @@ function retrieveClassString(string){
 }; // Terrible solution holding everything together. TESTED AND WORKS
 
 function parseClassNamesIntoArray(string){
-  return retrieveClassString(string).split(String.fromCharCode(32)); //CharCode 32 is space.
+  return retrieveClassString(string).split(String.fromCharCode(32));
 }; //TESTED AND WORKS
 
 function searchClassNameArrayForContainer(string, file){
-  return parseClassNamesIntoArray.includes(`${nameOfComponent(file)}-container`);
+  return parseClassNamesIntoArray.includes(`-container`);
 }; //UNTESTED
 
 function searchClassNameArrayForItem(string, file){
@@ -73,124 +70,130 @@ function findWritePosition(string){
 }; // UNTESTED
 
 function kebabCaseToCamelCase(string){
-  return string.replace(/-([a-z])/g, function(s, group1){
-      return group1.toUpperCase();
+  return string.replace(/-([a-z])/g, function(s, captured){
+      return captured.toUpperCase();
     });
   }; // TESTED AND WORKS
 
 function camelCaseToKebabCase(string){
-  return string.replace(/([A-Z])/g, function(s, group1){
-        return "-"+group1.toLowerCase();
+  return string.replace(/([A-Z])/g, function(s, captured){
+        return "-"+captured.toLowerCase();
       });
     }; // TESTED AND WORKS
 
-function appendContainerDirectives(string, file){
+function appendContainerDirectives(string){
   // THIS IS ALL WRONG. NAME CANNOT BE DERIVED FROM FILE. MULTIPLED ELEMENTS WITHIN A TEMPLATE.
-  // return containerDirectivesString =
-  //   `
-  //   [fxLayout]=\"${nameOfComponent(file)}Layout\"\r\n
-  //   [fxLayoutAlign]=\"${nameOfComponent(file)}LayoutAlign\"\r\n
-  //   [fxLayoutWrap]=\"${nameOfComponent(file)}LayoutWrap\"\r\n
-  //   [fxLayoutGap]=\"${nameOfComponent(file)}LayoutGap\"\r\n
-  //   `
+  return containerDirectivesString =
+    `
+    ${string}
+    [fxLayout]=
+    [fxLayoutAlign]=
+    [fxLayoutWrap]=
+    [fxLayoutGap]=
+    `
 }; // NEED TO INCORPORATE findWritePosition
 
-function appendItemDirectives(string, file){
+function appendItemDirectives(string){
   // THIS IS ALL WRONG. NAME CANNOT BE DERIVED FROM FILE. MULTIPLED ELEMENTS WITHIN A TEMPLATE.
-  // return itemDirectiveString =
-  //   `
-  //   [fxFlex]=\"${nameOfComponent(file)}Flex\"\r\n
-  //   [fxFlexOrder]=\"${nameOfComponent(file)}FlexOrder\"\r\n
-  //   [fxFlexOffset]=\"${nameOfComponent(file)}FlexOffset\"\r\n
-  //   [fxFlexAlign]=\"${nameOfComponent(file)}FlexAlign\"\r\n
-  //   [fxFlexFill]=\"${nameOfComponent(file)}FlexFill\"\r\n
-  //   `
+  return itemDirectiveString =
+    `
+    ${string}
+    [fxFlex]=
+    [fxFlexOrder]=
+    [fxFlexOffset]=
+    [fxFlexAlign]=
+    [fxFlexFill]=
+    `
 }; // NEED TO INCORPORATE findWritePosition
 
-function lineStartsHTMLElement(string){
+function lineStartsHTMLElement(string){};
+
+function classifyingFunction(string){
+
+
+  if(findContainerClass(string) && findItemClass(string)){
+    return "container item"
+
+  }
+  else if (findContainerClass(string)){
+    return "container"
+  }
+  else if (findItemClass(string)){
+    return "item"
+  }
+  else {
+    return "none"
+
+  }
 
 };
 
-console.log(kebabCaseToCamelCase("name-container-lame-derp"));
-console.log(camelCaseToKebabCase(kebabCaseToCamelCase("name-container-lame-derp")));
+function containerItemString(string){
+  return itemString(containerString(string));
+};
 
-// If name-container -> add container properties as nameContainerProperty
-// If name-item -> add item properties as nameItemProperty
+function containerString(string){
 
-// DIRECTIVES FOR DOM CONTAINERS
-// fxLayout	row | column | row-reverse | column-reverse
-// fxLayoutAlign	start|center|end|space-around|space-between start|center|end|stretch
-// fxLayoutWrap	"" | wrap | none | nowrap | reverse
-// fxLayoutGap	%, px, vw, vh
+};
 
-/*
-[fxLayout]=""
-[fxLayoutAlign]=""
-[fxLayoutWrap]=""
-[fxLayoutGap]=""
-*/
+function itemString(string){
 
-// DIRECTIVES FOR DOM ELEMENTS
-// fxFlex	"" , px , %, vw, vh, " ",
-// fxFlexOrder	int
-// fxFlexOffset	%, px, vw, vh
-// fxFlexAlign	start|baseline|center|end
-// fxFlexFill
+};
 
-/*
-[fxFlex]=""
-[fxFlexOrder]=""
-[fxFlexOffset]=""
-[fxFlexAlign]=""
-[fxFlexFill]=""
-*/
+function theMagic(string){
 
-// BOTH CONTAINERS AND ELEMENTS
-// fxHide	TRUE, FALSE, 0, ""
-// fxShow	TRUE, FALSE, 0, ""
-// ngClass	@see ngClass
-// ngStyle	@see ngStyle
 
-/*
-[fxHide]=""
-[fxShow]=""
-[ngClass]=""
-[ngStyle]=""
-*/
+
+  switch(classifyingFunction(string)){
+
+    case 'container item':
+
+      return appendItemDirectives(appendContainerDirectives(string));
+      break;
+
+    case 'container':
+
+      return appendContainerDirectives(string);
+      break;
+
+    case 'item':
+
+      return appendItemDirectives(string);
+      break;
+
+    case 'none':
+
+      return string;
+      break;
+
+    default:
+      console.log(`You missed a case somehow. Classification: ${classifyingFunction(string)} String: ${string}`);
+      throw Error;
+      break
+  }
+};
+
 
 exports.add = function(file) {
   console.log(`add html`);
 };
 
 exports.change = function(file) {
-  console.log("change html");
-  // fs.readFile( templatePath(file) , 'utf8', ( err , data ) => {
 
-  //     let lines = data.split('\n'); // each line of the template file.
-  //     // Find class strings. HAS CLASS STRING OR DOESN'T HAVE CLASS STRING.
-  //     let classStrings = [].push()
-  //     /*
-
-  //     CLASS STRING FUNCTIONS
-
-  //     HAS CLASS STRING ->
-  //       IS CONTAINER && IS ITEM ->
-  //         Add container directives and add item directives
-  //       ISNT CONTAINER && ISNT ITEM ->
-  //         Do nothing
-  //       IS CONTAINER && ISN'T ITEM ->
-  //         Add container directives only
-  //       ISNT CONTAINER && IS ITEM ->
-  //         Add item directives only
-
-  //     */
+  const route = "./gulp_support/testing.component.html";
+  fs.readFile( route , 'ascii', (err, data)=>{
+    if (err) throw err;
+    else {
+      let lines = data.split('\n');
+      let newLines = lines.map(x => theMagic(x));
+      let updated = newLines.map(x => x.concat('\n')).reduce(( x , y ) => x + y );
+        fs.writeFile( route, updated, (err) => {
+          if (err) throw err;
+        });
+    };
+});
 
 
-
-  //     let updated = lines.map(x => x.concat('\n')).reduce(( x , y ) => x + y );
-  //       fs.writeFile( route_to_themesList, updated, (err) => {
-  //           if (err) throw err;
-  //         });
 };
 
 exports.unlink = function(file) {
